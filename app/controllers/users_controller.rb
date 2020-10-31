6e0eaed6e71df626326999abdac64d67addc000f
uk_tech <spring_p0012015@yahoo.co.jp>
2020-10-31 12:16:23 +0000

    favorite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-  
-  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers] 
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :likes] 
   
   def index
     @users = User.order(id: :desc).page(params[:page]).per(10)
@@ -39,7 +38,13 @@ class UsersController < ApplicationController
     @followers = @user.followers.page(params[:page])
     counts(@user)
   end
-
+  
+  def likes
+    @user = User.find(params[:id])
+    @like_microposts = @user.like_microposts.page(params[:page])
+    counts(@user)
+  end
+  
   private
   
   def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,18 +6,21 @@ class User < ApplicationRecord
                       uniqueness: { case_sensitive: false }
     has_secure_password
     has_many :microposts
+    
     has_many :relationships
     has_many :followings, through: :relationships, source: :follow
     has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
     has_many :followers, through: :reverses_of_relationship, source: :user
     
+    has_many :favorites
+    has_many :like_microposts, through: :favorites, source: :micropost
+
     def follow(other_user)
         unless self == other_user
             self.relationships.find_or_create_by(follow_id: other_user.id)
         end
     end
-    
-    
+
     def unfollow(other_user)
         relationship = self.relationships.find_by(follow_id: other_user.id)
         relationship.destroy if relationship
@@ -25,6 +28,23 @@ class User < ApplicationRecord
     
     def following?(other_user)
         self.followings.include?(other_user)
+    end
+    
+    def feed_microposts
+        Micropost.where(user_id: self.following_ids + [self.id])
+    end
+    
+    def favorite(other_micropost)
+        self.favorites.find_or_create_by(micropost_id: other_micropost.id)
+    end
+    
+    def unfavorite(other_micropost)
+        favorite = self.favorites.find_by(micropost_id: other_micropost.id)
+        favorite.destroy if favorite
+    end
+    
+    def favorite?(other_micropost)
+        self.like_microposts.include?(other_micropost)
     end
 
 end
